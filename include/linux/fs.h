6112a6d70f7beb29e8c2f67c60b86fd04cc22608
sub77 <sub77@ymail.com>
2017-02-28 11:28:07 +0000

    Revert "Revert "misc: Import SM-G900F kernel source code""
    This reverts commit 670a5e9d13c4295b78efffeb6fef62a007380ba6.

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -1600,7 +1600,6 @@ extern int vfs_link(struct dentry *, struct inode *, struct dentry *);
 extern int vfs_rmdir(struct inode *, struct dentry *);
 extern int vfs_unlink(struct inode *, struct dentry *);
 extern int vfs_rename(struct inode *, struct dentry *, struct inode *, struct dentry *);
-extern long do_unlinkat(int, const char __user *, bool);
 
 /*
  * VFS dentry helper functions.
@@ -1764,7 +1763,7 @@ struct super_operations {
 	int (*bdev_try_to_free_page)(struct super_block*, struct page*, gfp_t);
 	int (*nr_cached_objects)(struct super_block *);
 	void (*free_cached_objects)(struct super_block *, int);
-	long (*unlink_callback)(struct inode *, char *);
+	long (*unlink_callback)(struct super_block *, char *);
 };
 
 /*
