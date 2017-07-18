cc7d39921f3e6e527ed0e80aec65e6db1bd065e3
Viper <vipergothot@hotmail.com>
2017-07-18 15:35:26 -0500

    error

--- drivers/media/platform/msm/camera_v2/pproc/cpp/msm_cpp.c
+++ drivers/media/platform/msm/camera_v2/pproc/cpp/msm_cpp.c
@@ -1414,7 +1414,6 @@ static int msm_cpp_cfg(struct cpp_device *cpp_dev,
 		      (cpp_frame_msg[12] & 0x3FF);
 			  fw_version_1_2_x = 0;
 	}
-				
 	if ((cpp_dev->hw_info.cpp_hw_version == CPP_HW_VERSION_1_1_0) ||
 	    (cpp_dev->hw_info.cpp_hw_version == CPP_HW_VERSION_1_1_1)) {
 		fw_version_1_2_x = 2;
